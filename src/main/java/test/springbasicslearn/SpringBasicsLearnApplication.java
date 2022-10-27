3ffa11fec9c9fa49cd47e44e4b82ac06c88c5bd9
Andrey Klodt <andreyklodt@gmail.com>
2022-10-27 07:27:34 +0300

    Init

---
+++ src/main/java/test/springbasicslearn/SpringBasicsLearnApplication.java
@@ -0,0 +1,13 @@
+package test.springbasicslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicsLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/springbasicslearn/SpringBasicsLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package test.springbasicslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicsLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
